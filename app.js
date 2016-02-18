b7ea446f0d9510ebfb0cdb5573076f7a44c4d074
Llong1 <137539449@qq.com>
2016-02-18 16:55:47 +0800

    调整结构

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function initApp(app, port){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.set('view options', {
+        'open': '{{',
+        'close': '}}'
+    });
+    app.engine('.html', ejs.__express);
+    app.use(express.static('public'));
+    // app.use(express.session());
+    // app.use(express.urlencoded());
+}
+
+var express = require('express')
+  , app = express();
+var ejs = require('ejs');
+var config = require('./config-sample');
+
+var port = config._DEBUG ? 8888 : 18080;
+
+initApp(app, port);
+
+// 对网站首页的访问返回 "Hello World!" 字样
+app.get('/', function (req, res) {
+    res.render('test', { title: 'Hey', message: 'Hello there!'});
+});
+
+var server = app.listen(port, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log('Example app listening at http://%s:%s', host, port);
+});
